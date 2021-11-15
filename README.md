76f9150f3c8b858b5dad800da0708fbb5a297563
hpurring <hilarypurrington@gmail.com>
2021-11-14 21:45:44 -0500

    updates and readme

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# weather-dashboard
+
+# Weather Dashboard
+*by Hilary Purrington*
+
+
+## **Description**
+This project demonstrates how I can build a basic weather dashboard using a third-party API; save previous searches to and retrieve from local storage; and implement Bootstrap.
+
+View my page [here](https://hpurring.github.io/weather-dashboard/).
+
+
+![landing page screenshot](assets/weather-dashboard-screenshot.png) 
+
+## **Installation**
+None required.
+
+## **Usage**
+To provide weather updates and 5-day forecasts from locations around the world.
+
+## **Credits**
+No partners.
+
+## **License**
+
+Copyright (c) 2021 by Hilary Purrington
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/script.js
+++ assets/script.js
@@ -23,7 +23,7 @@ var getCurrentWeather = function() {
       .catch(function(error) {
         alert('Unable to connect to OpenWeather.');
       });
-  };
+};
 
 var searchEl = document.querySelector("#search");
 var cityInputEl = document.querySelector("#city-name");
@@ -47,12 +47,13 @@ var searchHandler = function(event) {
 //     if city_list doesn't exist => create a new array, push the items and set in local storage
 //     else => use the city_list got from storage, push new item and set in local storage
     function saveSearch() {
-        if (!savedItems) {
+        var cityList = JSON.parse(localStorage.getItem("City-List"));
+        if (!cityList) {
             savedItems.push(cityName);        
             localStorage.setItem("City-List", JSON.stringify(savedItems));
         } else {
-            cityList = localStorage.getItem("City-List");
-            savedItems.push(cityList);
+            cityList = JSON.parse(localStorage.getItem("City-List"));
+            savedItems = cityList;
             savedItems.push(cityName);        
             localStorage.setItem("City-List", JSON.stringify(savedItems));
         };
@@ -72,18 +73,21 @@ function displayWeather(data) {
     console.log(temp);
     console.log(wind);
     console.log(humidity);
+
+    var headerEl = document.getElementById("header");
+    headerEl.innerHTML = "5-Day Forecast";
     
     var tempEl = document.getElementById("temp");
-    tempEl.innerHTML = temp;
+    tempEl.innerHTML = "Temperature: " + temp + "°F";
 
     var windEl = document.getElementById("wind");
-    windEl.innerHTML = wind;
+    windEl.innerHTML = "Wind: " + wind + " mph";
 
     var humidityEl = document.getElementById("humidity");
-    humidityEl.innerHTML = humidity;
+    humidityEl.innerHTML = "Humidity: " + humidity + "%";
 
     var cityNameEl = document.getElementById("search-term");
-    cityNameEl.innerHTML = data.name + ", " + data.sys.country;
+    cityNameEl.innerHTML = "Current weather for: " + data.name + ", " + data.sys.country;
 
     if (data.length === 0) {
         cityInfoEl.textContent = "No city found.";
@@ -123,53 +127,164 @@ function getCityCoordinates(data) {
         alert('Unable to connect to OpenWeather.');
       });
 
+      datesArray = [];
+      
+
+
       function getDates(data) {
+        dateArray = [];
         for (let i = 0; i < 5; i++) {
-            var milliseconds = data.daily[0].dt * 1000;
+            var milliseconds = data.daily[i].dt * 1000;
             var dateObject = new Date(milliseconds);
             var date = dateObject.toLocaleString("en-US");
-            console.log(date);
+            dateArray.push(date);
         }
+
+        var date1 = document.getElementById("date1");
+        date1.innerHTML = dateArray[0];
+
+        var date2 = document.getElementById("date2");
+        date2.innerHTML = dateArray[1];
+
+        var date3 = document.getElementById("date3");
+        date3.innerHTML = dateArray[2];
+
+        var date4 = document.getElementById("date4");
+        date4.innerHTML = dateArray[3];
+
+        var date5 = document.getElementById("date5");
+        date5.innerHTML = dateArray[4];
+
       };
 
       function getTemp(data) {
-          for (let i = 0; i < 5; i++) {
-            var forecastTemp = Math.round(((data.daily[i].temp.day - 273.15) * 1.8) + 32);
-            console.log(forecastTemp);
-          }
+        tempArray = [];          
+        for (let i = 0; i < 5; i++) {
+        var forecastTemp = Math.round(((data.daily[i].temp.day - 273.15) * 1.8) + 32);
+        tempArray.push(forecastTemp);
+        }
+        console.log(tempArray);
+
+        var temp1 = document.getElementById("temp1");
+        temp1.innerHTML = "Temperature: " + tempArray[0] + "°F";
+
+        var temp2 = document.getElementById("temp2");
+        temp2.innerHTML = "Temperature: " + tempArray[1] + "°F";
+
+        var temp3 = document.getElementById("temp3");
+        temp3.innerHTML = "Temperature: " + tempArray[2] + "°F";
+
+        var temp4 = document.getElementById("temp4");
+        temp4.innerHTML = "Temperature: " + tempArray[3] + "°F";
+
+        var temp5 = document.getElementById("temp5");
+        temp5.innerHTML = "Temperature: " + tempArray[4] + "°F";
+
+        //   var tempEl = document.getElementById("temp");
+        //     tempEl.innerHTML = temp;
       }
 
       function getWind(data) {
+        windArray = [];
         for (let i = 0; i < 5; i++) {
-            var forecastWind = data.daily[0].wind_speed;
-            console.log(forecastWind);
+            var forecastWind = data.daily[i].wind_speed;
+            windArray.push(forecastWind);
             }
+        console.log(windArray);
+        
+        var wind1 = document.getElementById("wind1");
+        wind1.innerHTML = "Wind: " + windArray[0] + " mph";
+
+        var wind2 = document.getElementById("wind2");
+        wind2.innerHTML = "Wind: " + windArray[1] + " mph";
+
+        var wind3 = document.getElementById("wind3");
+        wind3.innerHTML = "Wind: " + windArray[2] + " mph";
+
+        var wind4 = document.getElementById("wind4");
+        wind4.innerHTML = "Wind: " + windArray[3] + " mph";
+        
+        var wind5 = document.getElementById("wind5");
+        wind5.innerHTML = "Wind: " + windArray[4] + " mph";
         };
 
     function getHumidity(data) {
+        humidityArray = [];
         for (let i = 0; i < 5; i++) {
-            var forecastHumidity = data.daily[0].humidity;
-            console.log(forecastHumidity);
-            }; 
+            var forecastHumidity = data.daily[i].humidity;
+            humidityArray.push(forecastHumidity);
+            };
+            console.log(humidityArray); 
+
+        var hum1 = document.getElementById("humidity1");
+        hum1.innerHTML = "Humidity: " + humidityArray[0] + "%";
+
+        var hum2 = document.getElementById("humidity2");
+        hum2.innerHTML = "Humidity: " + humidityArray[1] + "%";
+
+        var hum3 = document.getElementById("humidity3");
+        hum3.innerHTML = "Humidity: " + humidityArray[2] + "%";
+
+        var hum4 = document.getElementById("humidity4");
+        hum4.innerHTML = "Humidity: " + humidityArray[3] + "%";
+
+        var hum5 = document.getElementById("humidity5");
+        hum5.innerHTML = "Humidity: " + humidityArray[4] + "%";
+
         };
+
+
     };
 };
 
 function showPrevious() {
-    var previousSearches = [];
-    previousSearches.push(localStorage.getItem("City-List"));
-    for (x = 0; x < previousSearches.length; x++) {
-        $("#previous-searches").append("<button class='previous-search mt-1'>" + previousSearches[x] + "</button>");
+    var previousSearches = JSON.parse(localStorage.getItem("City-List"));
+    var savedSearchHeader = document.getElementById("saved-search-header");
+    savedSearchHeader.innerHTML = "Saved Searches";
+    if (previousSearches) {
+        
+    for (let x = 0; x < previousSearches.length; x++) {
+        $("#saved-searches").append("<button class='previous-search mt-1'>" + previousSearches[x] + "</button>");
+
     };
-};
+    } else {
+        console.log("No previous searches.");
+    }
+    
+    var searchButtons = document.getElementsByClassName("previous-search");
+    var buttonInfo = Array.from(searchButtons);
+    for (let i = 0; i < buttonInfo.length; i++) {
+        addEventListener('click', function() {
+            // format the github api url
+            var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + buttonInfo[i].innerHTML + '&appid=e58ce6fcd378144b93c4b6f45a5073c8';
+              
+            // make a get request to url
+            fetch(apiUrl)
+            .then(function(response) {
+            // request was successful
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function(data) {
+                    console.log(data);
+                    displayWeather(data)
+                    getCityCoordinates(data);
+                });
+                } else {
+                    alert('Error: ' + response.statusText);
+                    }
+                })
+                .catch(function(error) {
+                alert('Unable to connect to OpenWeather.');
+                });
+            });
+        }
+    };
+    // buttonInfo.forEach(v => v.addEventListener('click', function() {
+    //     console.log(buttonInfo);
+    // }));
 
-// function getSavedItems() {
-//     var savedSearchesEl = document.getElementById("saved-searches");
-//     var savedButton = document.createElement("button");
-//     savedButton.innterHTML = localStorage.getItem("City");
-//     savedSearchesEl.append(savedButton);
 
-// };
+
 
 searchEl.addEventListener("submit", searchHandler);
 showPrevious();
